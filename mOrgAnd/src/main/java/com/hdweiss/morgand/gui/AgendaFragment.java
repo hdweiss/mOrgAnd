c79fb1ae8afa1f55a59c072bb6673379bfb1e949
Henning Weiss <hdweiss@gmail.com>
2014-03-01 12:34:03 +0100

    Created basic gui structure.

---
+++ mOrgAnd/src/main/java/com/hdweiss/morgand/gui/AgendaFragment.java
@@ -0,0 +1,22 @@
+package com.hdweiss.morgand.gui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.hdweiss.morgand.R;
+
+public class AgendaFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        textView.setText("Agenda");
+        return rootView;
+    }
+}
---
+++ mOrgAnd/src/main/java/com/hdweiss/morgand/gui/OutlineFragment.java
@@ -0,0 +1,22 @@
+package com.hdweiss.morgand.gui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.hdweiss.morgand.R;
+
+public class OutlineFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        textView.setText("Outline");
+        return rootView;
+    }
+}
