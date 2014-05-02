b56344835bf3cf42a04dc999870e17bcc4091004
Henning Weiss <hdweiss@gmail.com>
2014-05-02 17:21:36 +0200

    Created producer for sync events.

--- mOrgAnd/src/main/java/com/hdweiss/morgand/Application.java
+++ mOrgAnd/src/main/java/com/hdweiss/morgand/Application.java
@@ -3,8 +3,11 @@ package com.hdweiss.morgand;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
+import com.hdweiss.morgand.synchronizer.SyncEvent;
 import com.hdweiss.morgand.synchronizer.SyncService;
 import com.squareup.otto.Bus;
+import com.squareup.otto.Produce;
+import com.squareup.otto.Subscribe;
 
 public class Application extends android.app.Application {
 
@@ -20,15 +23,27 @@ public class Application extends android.app.Application {
         return bus;
     }
 
+    private SyncEvent syncEvent = new SyncEvent(SyncEvent.State.Done);
+
+    @Produce public SyncEvent produceSyncEvent() {
+        return this.syncEvent;
+    }
+
+    @Subscribe public void syncEvent(SyncEvent event) {
+        this.syncEvent = event;
+    }
+
     public static SharedPreferences getPreferences() {
         return PreferenceManager.getDefaultSharedPreferences(instace);
     }
+
 
     @Override
     public void onCreate() {
         super.onCreate();
         instace = this;
         SyncService.startAlarm(this);
+        getBus().register(this);
     }
 
     @Override
